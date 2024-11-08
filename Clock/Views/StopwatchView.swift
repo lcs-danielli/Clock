5c26b2415015e7d5032ce4a15d207a15c837a778
Daniel Li <danli@lcs.on.ca>
2024-11-08 09:11:59 -0500

    Set the color scheme and tint

---
+++ Clock/Views/StopwatchView.swift
@@ -0,0 +1,18 @@
+//
+//  StopwatchView.swift
+//  Clock
+//
+//  Created by 李泽宇 on 2024-11-08.
+//
+
+import SwiftUI
+
+struct StopwatchView: View {
+    var body: some View {
+        Text("Stopwatch")
+    }
+}
+
+#Preview {
+    StopwatchView()
+}
